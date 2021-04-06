38c312e6e24c6471ab5873978b80400c605ded0e
YASHVI BANSAL <61915754+yashvibansalmin19@users.noreply.github.com>
2021-04-06 15:17:49 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=.\letustalk-8d73-Web-Deploy.pubxml -p:Password=Y2EAi84Kx3E2ih00cjgi8arwcKlbJPgehjAu8uDxtwWCjR3JxTk727aC3YBT
+
